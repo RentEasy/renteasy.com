a7cfe718db612989b030b3e0d751f20b06205e99
clone1018 <luke@axxim.net>
2020-06-07 13:12:08 -0400

    Adding Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,38 @@
     <meta name="twitter:description" content="@yield('description', config('app.description'))">
     <meta name="twitter:image" content="@yield('image', asset('img/pittsburgh-row-houses.jpg'))">
 
+
+    @if(config('services.google_analytics.id'))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.id') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '{{ config('services.google_analytics.id') }}');
+        </script>
+    @endif
+    @if(config('services.facebook.id'))
+    <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '{{ config('services.facebook.id') }}');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id={{ config('services.facebook.id') }}&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Facebook Pixel Code -->
+    @endif
+
     @yield('head')
 
 </head>
